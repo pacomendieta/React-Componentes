a65bb94bbdd45d6f5649918155a2c2eacddb1e51
josejfml <jfml2014@gmail.com>
2023-02-13 20:59:05 +0100

    Scroll con Autosizer

--- src/ListadoInfinito.jsx
+++ src/ListadoInfinito.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {Column, Table} from 'react-virtualized';
 import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
 */
+//import 'react-virtualized/styles.css'
 import {
   AutoSizer,
   CellMeasurer,
@@ -13,13 +14,42 @@ import {
 export const ListadoInfinito = ( props ) =>{
   console.log("Listado.tabla=", props.tabla)
 
+let estilos = {
+   "height":"2em"
+}
+let styleContenedor = {
+  "height":"300px",
+  "color": "white",
+  "backgroundColor":"black"
+}
+
 return (
  <>
   <h2>Scroll infinito</h2>
-  <List>
+  <div style={styleContenedor}>
+  <AutoSizer>{
+    ({width, height})=>( 
+      <List
+        height = {height /* altura utilizable en medidas CSS */}
+        width={width}
+        rowHeight={(n)=>24 /* altura fila???, numero o funcion: (index)=>num */ }   
+        rowCount = {24}
+        overscanRowCount={1 /*filas cacheadas al hacer scroll, def=10 */}
+        rowRenderer = { ({index, key, style, parent})=>{
+          return (
+            <div key={index}>
+              Elementos:{ props.tabla[index]? <>{props.tabla[index].firstName}</> : "-no--"}
+            </div>
+          );
+        } }
+      >
+    </List>
 
+    )
+  }
+  </AutoSizer>
 
-  </List>
+  </div>
   <h2>Listado Infinito </h2>
   <>
   {
