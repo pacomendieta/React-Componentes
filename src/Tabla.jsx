7e9ed438dabe1d86f0f8ae1d36a118200173e1d4
josejfml <jfml2014@gmail.com>
2023-03-08 20:59:46 +0100

    Tabla.jsx

--- src/Tabla.jsx
+++ src/Tabla.jsx
@@ -1,61 +1,85 @@
 import React,{useState, useEffect} from 'react';
 
-const USERS_URL = 'http://localhost:8001/personas';
-let x
-export default function Table () {
-  let [cargado,setCargado]=useState(false)
-  let paginaInicial={count:0,page:0,lastpage:0,results:[]}
-  let [paginaActual, setPaginaActual] = useState(paginaInicial)
+const USERS_URL =   'http://localhost:8002/personas';
 
+export default function Table () {
+
+const usePagina=()=>{
+  let paginaInicial={count:0,actualPage:0,lastPage:0,results:[], cargada:false}
+  let [pagina, setPagina] = useState( paginaInicial )
+  const cargarPersonas=( datos )=>{
+       setPagina( {actualPage:pagina.actualPage,results:[...datos.results], count:datos.count,  lastPage:Math.round(datos.count/10), cargada:true } )
+   }
+   const setActualPage=(p)=>setPagina( {...pagina, actualPage:p})
+   return [ pagina, cargarPersonas, setActualPage ]
+}
+let [pagina, cargarPersonas, setActualPage] = usePagina ()
+console.log("Estado:", pagina)
 
 useEffect(()=>{
-  if (!cargado) {
-    console.log("useEffect() porque paginaActual= ",paginaActual)
-    fetch (USERS_URL)
+    console.log("useEffect() ")
+    fetch (USERS_URL+"?page="+pagina.actualPage,{method:'GET'})
     .then ( (data)=>data.json() )
     .then ( (res)=>{ 
-      console.log("RES:", res)
-      //paginaInicial = JSON.parse( JSON.stringify( res )); 
-      paginaInicial = {results:[...res]}
-      console.log("DATA1:",paginaInicial)
-      //setPaginaActual( paginaInicial ) //setPaginaActual( paginaInicial )
-      setPaginaActual(paginaInicial)
-      console.log("Pagina Actual:",paginaActual)
-      setCargado(true)
+      console.log("CARGADO:", USERS_URL+"?page="+pagina.actualPage ,res)
+      //setPaginaActual(res)
+      //setCargado(true)
+      cargarPersonas(res)
     })
-  }
-},[ paginaActual.count ])
+    .catch (e => console.error("Error en fetch", e))
+},[ pagina.actualPage ])
 
- 
-  return (
-    <div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
+const Tabla = ()=>(
+  <div>
+  <table className="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+      </tr>
+    </thead>
+    <tbody>
+    {
+           pagina.results?.map((row,idx)=>
+          <tr key={idx}>
+            <td>{row.id}</td>
+            <td>{row.firstName}</td>
+            <td>{row.lastName}</td>
           </tr>
-        </thead>
-        {       console.log("Pagina Actual---:",paginaActual) }
-        <tbody>
-        {
-               paginaActual.results.map((row,idx)=>
-              <tr key={idx}>
-                <td>{row.id}</td>
-                <td>{row.firstName}</td>
-                <td>{row.lastName}</td>
-              </tr>
-            )
-        }
-        </tbody>
-      </table>
-      <section className="pagination">
-        <button className="first-page-btn">first</button>
-        <button className="previous-page-btn">previous</button>
-        <button className="next-page-btn">next</button>
-        <button className="last-page-btn">last</button>
-      </section>
-    </div>
-  );
-};
+        )
+    }
+    </tbody>
+  </table>
+</div>
+ )
+
+ const isDisabled=( )=>{
+
+ }
+ const hFirstPageBtn =()=>setActualPage( 0)
+ const hNextPageBtn  =()=>setActualPage( pagina.actualPage + 1)
+ const hPrevPageBtn  =()=>setActualPage( pagina.actualPage - 1)
+ const hLastPageBtn  =()=>setActualPage( pagina.lastPage )
+
+ const Botonera=()=>{
+   return (
+    <section className="pagination">
+    <button className="first-page-btn" disabled={!pagina.cargada || pagina.actualPage==0} onClick={hFirstPageBtn}>first</button>
+    <button className="previous-page-btn" disabled={!pagina.cargada || pagina.actualPage==0} onClick={hPrevPageBtn} >previous</button>
+    <button className="next-page-btn" disabled={!pagina.cargada || pagina.actualPage==pagina.lastPage}  onClick={hNextPageBtn}>next</button>
+    <button className="last-page-btn" disabled={!pagina.cargada || pagina.actualPage==pagina.lastPage} onClick={hLastPageBtn}>last</button>
+  </section>
+   )
+ }
+return ( 
+<>
+  {pagina.cargada? <Tabla /> : <p>Cargando...</p> }
+  <Botonera  />
+</> )
+
+
+    
+ 
+
+} //Table
